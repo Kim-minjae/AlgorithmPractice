ee6e65c9148bcef0cdcab9567004d8a6a3b6b0b7
Kim min-jae <pose2done@naver.com>
2017-10-03 14:44:11 +0900

    commit for pull

---
+++ src/samsungPro4/Main.java
@@ -0,0 +1,12 @@
+package samsungPro4;
+
+import java.io.IOException;
+
+/**
+ * Created by pose2 on 2017-10-01.
+ */
+public class Main {
+     public static void main(String args[])throws IOException{
+
+     }
+}
